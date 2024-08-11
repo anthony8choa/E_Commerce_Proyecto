c1ebbd7d21b55a244464ec7e680a741bc3d0df73
unknown <alejandrobgabrielc@gmail.com>
2024-08-11 01:21:08 -0600

    Actualización de mostrarProductoPorId, valor de retorno adicional -> seccion de comentarios

--- app/Http/Controllers/ProductosController.php
+++ app/Http/Controllers/ProductosController.php
@@ -9,7 +9,7 @@ class ProductosController extends Controller
 {
     /**
      * Este método retorna 1 producto mandando el id del producto a la api y retornando los datos del producto
-     * para mostrarlo en la vista 'visualizarProducto'
+     * para mostrarlo en la vista 'visualizarProducto', NUEVO: incluyendo la seccion de comentarios asociada a dicho producto
      * @param idProducto refiere al id del producto a buscar
      * @return view refiere a la vista a la cual se le enviará todos los datos de ese producto
      */
