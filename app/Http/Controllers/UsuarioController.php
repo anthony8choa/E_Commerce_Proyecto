6fbf5e95abf23705e816f67e9ebcd83db9cd53e7
unknown <alejandrobgabrielc@gmail.com>
2024-08-11 01:18:31 -0600

    Se añadieron 2 controladores en UsuariosController

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -21,4 +21,25 @@ class UsuarioController extends Controller
         //$request->nombreUsuario,
         //$request->contrasenia
     }
+
+    /**
+     * Esta funcion obtiene toda la información asociada al usuario, es decir:
+     * tabla usuario, sus lugares asociados,
+     * sus metodos de pago, las compras de ese usuario (osea todo lo que ha comprado)
+     * , en conclusion todo menos sus favoritos (ya que se mostrara en otra vista)
+     * @param idUsuario Refiere al idUsuario a mandar para recibir sus atributos
+     * @return . Refiere al retorno de dicho datos (nose muy bien como se hará este método, cualquier retorno es valido, ya sea lista o api personalizada con dichos valores)
+     */
+    public function obtenerUsuarioConAtributos($idUsuario){
+
+    }
+
+    /**
+     * Obtiene la lista de favoritos de un usuario, se envía el usuario y recibe su lista de productos
+     * @param idUsuario Refiere al id del usuario a enviar para recibir su lista
+     * @return listaFavoritos Refiere a la lista de productos asociada al usuario
+     */
+    public function obtenerListaFavoritos($idUsuario){
+
+    }
 }
